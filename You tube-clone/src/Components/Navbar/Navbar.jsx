2ee9bb363bb1fc22b56adebef397c86cf0c0fa70
KeerthiKrish8 <Keerthikak0393@gmail.com>
2025-07-25 09:28:16 +0530

    add

---
+++ You tube-clone/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Navbar.css';
+import logo from "./youtube-logo2.png";
+function Navbar(){
+    return(
+        <div className="Container_Navbar">
+           <div className="Logo_Navbar" >
+            <div className="burger" >
+                <p> </p>
+                <p> </p>
+                <p> </p>
+            </div>
+            <div className="logo_div_Navbar">
+                <img src={logo}alt=""
+                height={50}
+                width={150}/>
+                <p className="logo_title_navbar">YouTube</p>
+            </div>
+
+           </div>
+        </div>
+    );
+}
+
+export default Navbar;  
